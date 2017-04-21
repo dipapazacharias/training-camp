eaa78f53e82497a1e33e856355ed068bfcfa4220
Dimitris Papazacharias <dipapazacharias@gmail.com>
2017-04-21 20:47:16 +0300

    Copmlete simplest server

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const host = "127.0.0.1";
+const port = 8000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  res.end("Hello, world!\n");
+}).listen(port, host, () => {
+  console.log("Server listening at http://${hostname}:${port}");
+});
