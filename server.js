9b7513fc11662e619d2e4809d51d5320d374a0b7
Dimitris Papazacharias <dipapazacharias@gmail.com>
2017-04-29 16:13:33 +0300

    Completed branch development

--- server.js
+++ server.js
@@ -16,14 +16,45 @@ const mimeTypes = {
 
 const server = http.createServer((req, res) => {
   let uri = url.parse(req.url).pathname;
-  let fileName = path.join(process.cwd(), decodeURI(uri));
   console.log(`Loading "${uri}"` + "\n");
+
+  let fileName = path.join(process.cwd(), decodeURI(uri));
+
+  console.log(process.cwd());
+  console.log(decodeURI(uri));
   console.log(`Filename: "${fileName}"\n`);
   let stats;
 
-  res.writeHead(200, {"Content/type": "text/plain"});
-  res.end("Good morning!");
-}).listen(port, "localhost", () => {
+  try {
+    stats = fs.lstatSync(fileName);
+  } catch(e) {
+    res.writeHead(404, {"Content-type": "text/plain"});
+    res.write("404 Not Found\tbaby!!\n");
+    res.end();
+    return
+  }
+
+  // res.writeHead(200, {"Content/type": "text/plain"});
+  // res.end("Good morning!");
+
+  if (stats.isFile()) {
+    var mimeType = mimeTypes[path.extname(uri).split(".")[1]];
+    console.log("Requested file MIME-type: " + mimeType);
+    res.writeHead(200, {"Content/type": mimeType});
+    var fileStream = fs.createReadStream(fileName);
+    fileStream.pipe(res);
+  } else if (stats.isDirectory()) {
+    res.writeHead(302, {
+      "location": "public/index.html"
+    });
+    res.end();
+  } else {
+    res.writeHead(500, {"Content-type": "text/plain"});
+    res.write("500 Internal Error");
+    res.end();
+  }
+})
+.listen(port, "localhost", () => {
   console.log("Listening on localhost, port: " + port + "\n");
 });
 
