3035bde8eeea13052acabcd8f1e0d17d4f22a2bc
Dimitris Papazacharias <dipapazacharias@gmail.com>
2017-05-06 21:40:12 +0300

    Completed nodemailer implementation

--- app.js
+++ app.js
@@ -25,7 +25,36 @@ app.get("/contact", function(req, res){
   res.render("contact");
   console.log("Requesting \"contact\" handled ok");
 });
+app.post("/contact/send", function(req, res){
+  console.log("POST request");
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "dipapazach@gmail.com",
+      pass: "natoor66dt1985?"
+    }
+  });
 
+  let mailOptions = {
+    from: "Mitso Papazacho <dipapazach@gmail.com>",
+    to: "dipapazacharias@gmail.com",
+    subject: "Nodemailer test",
+    text: "You have a sumbission with the following details...\n Name: "+req.body.name+"\nEmail: "+req.body.email+"\nMessage: "+req.body.message,
+    html: "<p>You have a sumbission with the following details...</p><ul><li>Name: "+req.body.name+"</li><li>Email: "+req.body.email+"</li><li>Message: "+req.body.message+"</li></ul>"
+  }
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error) {
+      console.log(error);
+      res.redirect("/");
+    } else {
+      console.log("Email delivered successfully!!");
+      console.log(info);
+      res.redirect("/");
+    }
+  });
+
+});
 
 
 const port = 8001;
