84142f614ad5892fbad209c2d8aaf11005a7e6c2
Dimitris Papazacharias <dipapazacharias@gmail.com>
2017-05-06 17:20:12 +0300

    Copmleted basic pages and routes implementation

--- app.js
+++ app.js
@@ -5,17 +5,28 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "pug");
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
 app.get("/", function(req, res){
-  res.render("index");
-  console.log("Request handled ok");
+  res.render("index", { title: "Welcome"});
+  console.log("Requesting \"index\" handled ok");
 });
+app.get("/about", function(req, res){
+  res.render("about");
+  console.log("Requesting \"about\" handled ok");
+});
+app.get("/contact", function(req, res){
+  res.render("contact");
+  console.log("Requesting \"contact\" handled ok");
+});
+
+
 
 const port = 8001;
 app.listen(port);
