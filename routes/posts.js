03a74bcfa2efbc79840d4b1a37dedef56fd11da9
dipapaz <dipapazacharias@gmail.com>
2017-08-09 17:48:56 +0300

    truncate text and category view

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('monk')('localhost/nodeblog');
 var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var upload = multer({ dest: './public/images' });
 
 
 router.get('/add', function(req, res, next) {
